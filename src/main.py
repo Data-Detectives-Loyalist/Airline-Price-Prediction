db8b3ea510cdcb30394bae3d08f66326676ccba9
gaurav singh <gauravsingh.g809@gmail.com>
2024-06-05 13:30:24 -0400

    Performed Lasso, Ridge, Elastic Net

--- src/main.py
+++ src/main.py
@@ -14,7 +14,10 @@ def main():
     X_train, X_test, y_train, y_test = preprocess_data(data, target_column)
 
     # Train and evaluate models
-    results = train_and_evaluate_models(X_train, X_test, y_train, y_test)
+    results = train_and_evaluate_models(X_train, y_train)
+
+    # Print or save results
+    print(results)
 
     # Print results
     for model_name, metrics in results.items():
